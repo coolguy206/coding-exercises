23a12c9095b31a11d834645d2bcc5f2b34aaab50
David Thanphilom <davidthanphilom@msn.com>
2015-11-07 12:24:59 -0800

    moved constructors and prototypes into a object youngHuman

--- index.php
+++ index.php
@@ -5,26 +5,19 @@
 <title>Coding exercise</title>
 <script>
 // QUESTION #1
-var Baby = function(babyName, age, gender, race, species) {
+var youngHuman = {};
+youngHuman.Baby = function(babyName, age, gender, race, species) {
 	this.babyName = babyName;
 	this.age = age;
 	this.gender = gender;
 	this.race = race;
 	this.species = 'human';
 }
-Baby.prototype.cry = function() {
+youngHuman.Baby.prototype.cry = function() {
 	console.log(this.babyName + " is crying!");
 };
 
-var riley = new Baby('Riley', '5 months', 'male', 'asian');
-console.log(riley.babyName + ' is a ' + riley.species);
-riley.cry();
-
-var melani = new Baby('Melani', '1yr', 'female', 'asian');
-console.log(melani.babyName + ' is a ' + melani.species);
-melani.cry();
-
-var Toddler = function(babyName, age, gender, race, walk, solidFood) {
+youngHuman.Toddler = function(babyName, age, gender, race, walk, solidFood) {
 	this.babyName = babyName;
 	this.age = age;
 	this.gender = gender;
@@ -33,9 +26,17 @@ var Toddler = function(babyName, age, gender, race, walk, solidFood) {
 	this.solidFood = solidFood;
 }
 
-Toddler.prototype =  new Baby();
+youngHuman.Toddler.prototype =  new youngHuman.Baby;
 
-var kennedy = new Toddler('Kennedy', '3yr', 'female', 'asian', true, true);
+var riley = new youngHuman.Baby('Riley', '5 months', 'male', 'asian');
+console.log(riley.babyName + ' is a ' + riley.species);
+riley.cry();
+
+var melani = new youngHuman.Baby('Melani', '1yr', 'female', 'asian');
+console.log(melani.babyName + ' is a ' + melani.species);
+melani.cry();
+
+var kennedy = new youngHuman.Toddler('Kennedy', '3yr', 'female', 'asian', true, true);
 console.log(kennedy.babyName + ' is a ' + kennedy.species);
 kennedy.cry();
 
