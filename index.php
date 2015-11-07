bd66aacd895fee10c966efed469b35258232fffe
David Thanphilom <davidthanphilom@msn.com>
2015-11-07 10:36:57 -0800

    added js

--- index.php
+++ index.php
@@ -3,6 +3,23 @@
 <head>
 <meta charset="UTF-8">
 <title>Coding exercise</title>
+<script>
+// QUESTION #1
+var baby = function(name, age, gender, race) {
+	this.name = riley;
+	this.age = age;
+	this.gender = gender;
+	this.race = race;
+}
+baby.prototype.cry = function() {
+	console.log("I'm crying!");
+};
+
+var riley = new baby('Riley', '5 months', 'male', 'asian');
+riley.cry();
+
+var melani = new baby('Melani', '1yr', 'female', 'asian');
+</script>
 </head>
 
 <body>
