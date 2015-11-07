22c0e4325b5b23492f880bad6c375635cbe74537
David Thanphilom <davidthanphilom@msn.com>
2015-11-07 10:49:23 -0800

    fixed js

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <title>Coding exercise</title>
 <script>
 // QUESTION #1
-var baby = function(name, age, gender, race) {
-	this.name = riley;
+var baby = function(babyName, age, gender, race) {
+	this.babyName = babyName;
 	this.age = age;
 	this.gender = gender;
 	this.race = race;
@@ -19,6 +19,7 @@ var riley = new baby('Riley', '5 months', 'male', 'asian');
 riley.cry();
 
 var melani = new baby('Melani', '1yr', 'female', 'asian');
+melani.cry();
 </script>
 </head>
 
