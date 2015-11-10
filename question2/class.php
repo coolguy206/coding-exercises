45b28ab3a3bc373aad90aa27809956e4815ecd33
David Thanphilom <davidthanphilom@msn.com>
2015-11-10 13:48:28 -0800

    modified php code

--- question2/class.php
+++ question2/class.php
@@ -1,40 +1,32 @@
 <?php
-class Users {
+class Login {
 	public $userName;
-	public $userPassword;
+	public $userPassword; 
 	public function __construct($userName, $userPassword) {
         $this->userName = $userName;
     	$this->userPassword = $userPassword;
-    }
-    
+    }      
 }
 
-class newUsers extends Users {
-	public $userName = 'riley';
-	public $userPassword = 'riley415';
+class Users extends Login {
+		
 }
 
 $david = new Users('david', 'david415');
-$riley = new newUsers('riley', 'riley415');
-
-setcookie($david->userName, $david->userPassword);
-setcookie($riley->userName, $riley->userPassword);
-
 
 if($_SERVER['REQUEST_METHOD'] == "POST") {
-	$user = htmlentities($_REQUEST['user']);
-	$secret = htmlentities($_REQUEST['secret']);
 
 	if($david->userName == $user && $david->userPassword == $secret) {
-		echo '<p class="success">SUCCESS! YOU HAVE LOGGED IN ' . strtoupper($david->userName) . '!</p>';
+		echo 'SUCCESS! YOU HAVE LOGGED IN ' . strtoupper($david->userName);
+		setcookie($david->userName, $david->userPassword);
 	} else {
-		echo '<p class="fail">Oh something went wrong, please try again.</p>';
+		echo 'Oh something went wrong, please try again.';
 	}
 
 	
 }
 
-// print_r($david);
-// print_r($riley);
+// print_r($_SERVER);
 
+// echo 'you did it ' . $david->userName . '!';
 ?>
