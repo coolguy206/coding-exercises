e591913eaf8b7f1d8b8485500707adb3ebc229a5
David Thanphilom <davidthanphilom@msn.com>
2015-11-10 13:53:58 -0800

    modified js

--- question2/scripts.js
+++ question2/scripts.js
@@ -1,8 +1,7 @@
 // JAVASCRIPT VERSION OF QUESTION #2
 // ---------------------------------------------------------
 
- $.removeCookie('david');
- $.removeCookie('riley');
+ // $.removeCookie('david');
 
 $(document).ready(function(){
 	var Users = {};
@@ -20,33 +19,14 @@ $(document).ready(function(){
 
 	$('#existing').submit(function(){
 		$.post('class.php', function(data){
-		var container = $('<div></div>');
-		$('#existing').after(container);
-		container.text(data);
-		
+			var container = $('<div></div>');
+			$('#existing').after(container);
+			container.text(data)
+			.delay(3000)
+			.fadeOut(function(){
+				$(this).remove();
+			});
 		});
 		return false;
 	});
-	
-
-
-
-
-	/*$('#existing input[type="submit"]').click(function(){
-		var userName = $('#existing input[name="user"]').val();
-		var userPassword = $('#existing input[name="secret"]').val();
-		var container = $('<div></div>');
-		if(userName === Users.david.user && userPassword === Users.david.pass) {
-			// console.log('success');
-			$('#existing').after(container);
-			container.text('SUCCESS! YOU HAVE LOGGED IN ' + Users.david.user.toUpperCase() + '!').delay(3000).fadeOut(function(){$(this).remove();});
-			$.cookie(Users.david.user, Users.david.pass, {expires: 7});
-			// container.remove();
-		} else {
-			// console.log('fail');
-			$('#existing').after(container);
-			container.text('Oh something went wrong, please try again.').delay(3000).fadeOut(function(){$(this).remove();});
-		}
-		return false;
-	});*/
 });
